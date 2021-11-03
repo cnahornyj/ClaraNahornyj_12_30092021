6c830b622816c22269d9315b0d9d4dd8611af8d0
Clara <c.nahornyj@hotmail.fr>
2021-11-03 14:08:40 +0100

    Mise à jour des données via la data récupérée du composant parent

--- frontend/src/components/Performances.js
+++ frontend/src/components/Performances.js
@@ -7,86 +7,46 @@ import {
   Radar
 } from "recharts";
 
-const data = [
-  {
-    "subject": "cardio",
-    "A": 80
-  },
-  {
-    "subject": "energy",
-    "A": 120
-  },
-  {
-    "subject": "endurance",
-    "A": 140
-  },
-  {
-    "subject": "strength",
-    "A": 50
-  },
-  {
-    "subject": "speed",
-    "A": 200
-  },
-  {
-    "subject": "intensity",
-    "A": 150
-  }
-]
-const USER_PERFORMANCE = [
-  {
-      userId: 12,
-      kind: {
-          1: 'cardio',
-          2: 'energy',
-          3: 'endurance',
-          4: 'strength',
-          5: 'speed',
-          6: 'intensity'
-      },
-      data: [
-          {
-              value: 80,
-              kind: 1
-          },
-          {
-              value: 120,
-              kind: 2
-          },
-          {
-              value: 140,
-              kind: 3
-          },
-          {
-              value: 50,
-              kind: 4
-          },
-          {
-              value: 200,
-              kind: 5
-          },
-          {
-              value: 90,
-              kind: 6
-          }
-      ]
-  }
-]
-
-// extraire this.data.sessions
-// appliquer une méthode pour remplacer les dates par 1,2,3 etc
-// propsTypes array
-// utiliser les params pour trouver l'id
- 
 class Performances extends Component {
+  constructor(props){
+    super(props);
+    this.data = this.props.data;
+    this.newData = this.data.map((data) => {
+      switch (data.kind) {
+        case 1:
+          return { disciplined: "cardio", ...data };
+          break;
+        case 2:
+          return { disciplined: "energy", ...data };
+          break;
+        case 3:
+          return { disciplined: "endurance", ...data };
+          break;
+        case 4:
+          return { disciplined: "strength", ...data };
+          break;
+        case 5:
+          return { disciplined: "speed", ...data };
+          break;
+        case 6:
+          return { disciplined: "intensity", ...data };
+          break;
+        default:
+          return { ...data };
+      }
+    }); 
+    this.newData.forEach(function (item) {
+      delete item.kind;
+    });
+  }
   render() {
     return (
       <section className="performances">
-        <RadarChart width={256} height={261} data={data}>
+        <RadarChart width={256} height={261} data={this.newData}>
           <PolarGrid />
-          <PolarAngleAxis dataKey="subject" stroke="#fff"/>
+          <PolarAngleAxis dataKey="disciplined" stroke="#fff"/>
           <Radar
-            dataKey="A"
+            dataKey="value"
             stroke="#e60000"
             fill="#e60000"
             fillOpacity={0.6}
